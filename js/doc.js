07290fe1eee741edc5e5069dd77b72460fa54f34
SpektralDevelopment <spektraldevelopment@gmail.com>
2013-11-02 12:44:32 -0400

    Added pertinent information to documentation.json, in the middle of parsing JSON to built glossary list.

--- js/doc.js
+++ js/doc.js
@@ -2,11 +2,41 @@
 (function(){
 
     //vars
+    var
+        jsonObject = {};
+
+    //Comment for production
+    Spektral.debug();
 
     init();
 
+    ////////////////////
+    ////INIT
+    ////////////////////
     function init() {
+        if(document.readyState === "complete") {
+            loadJSON(null);
+        }
+        else {
+            Spektral.attachEventListener(window, 'load', loadJSON);
+        }
+    }
 
+    ////////////////////
+    ////LOAD JSON
+    ////////////////////
+    function loadJSON(e) {
+        Spektral.loadJSON("js/documentation.json", onJSONLoaded);
+    }
+
+    ////////////////////
+    ////ON JSON LOADED
+    ////////////////////
+    function onJSONLoaded(e) {
+
+        var key;
+        jsonObject = e;
+        //Spektral.log("JSON Loaded: " + Spektral.getInfo(jsonObject));
     }
 
 }(window));
