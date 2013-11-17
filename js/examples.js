f18fb650db0b369e1486aa8fbf0a2a4f0a69e34b
SpektralDevelopment <spektraldevelopment@gmail.com>
2013-11-17 12:10:18 -0500

    Added examples page, this page will be used for testing and demonstrating spektral.js.

---
+++ js/examples.js
@@ -0,0 +1,40 @@
+//Examples Spektral.js
+(function(){
+
+    //vars
+    var
+        jsonObject = {};
+
+    //Comment for production
+    Spektral.debug();
+
+    init();
+
+    ////////////////////
+    ////INIT
+    ////////////////////
+    function init() {
+        if(document.readyState === "complete") {
+            loadJSON(null);
+        }
+        else {
+            Spektral.attachEventListener(window, 'load', loadJSON);
+        }
+    }
+
+    ////////////////////
+    ////LOAD JSON
+    ////////////////////
+    function loadJSON(e) {
+        Spektral.loadJSON("js/documentation.json", onJSONLoaded);
+    }
+
+    ////////////////////
+    ////ON JSON LOADED
+    ////////////////////
+    function onJSONLoaded(e) {
+        jsonObject = e;
+        Spektral.log("Example.js json: " + Spektral.getInfo(jsonObject));
+    }
+
+}(window));
