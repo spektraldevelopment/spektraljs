12658334fcb3a7acb1cdf367a3399cafb1f4bb59
SpektralDevelopment <spektraldevelopment@gmail.com>
2013-11-17 12:15:27 -0500

    Added back in buildGlossary and related functions.

--- js/examples.js
+++ js/examples.js
@@ -34,7 +34,57 @@
     ////////////////////
     function onJSONLoaded(e) {
         jsonObject = e;
-        Spektral.log("Example.js json: " + Spektral.getInfo(jsonObject));
+        Spektral.removeElement(listLoading);
+        buildGlossary();
+    }
+
+    ////////////////////
+    ////BUILD GLOSSARY
+    ////////////////////
+    function buildGlossary() {
+
+        var key, catSection, catTitle;
+        for (key in jsonObject) {
+
+            catSection = Spektral.createNewElement("div", key + "Section", glossary);
+            Spektral.createSetAttribute(catSection, "class", "glossSection");
+
+            catTitle = Spektral.createNewElement("h2", "", catSection);
+            catTitle.innerHTML = key;
+            populateCategories(key, jsonObject[key], catSection);
+        }
+    }
+
+    ////////////////////
+    ////POPULATE CATEGORIES
+    ////////////////////
+    function populateCategories(cat, catObject, section) {
+
+        var key, catList, item, listItem, itemNum = 0;
+
+        catList = Spektral.createNewElement("ul", cat, section);
+
+        for (key in catObject) {
+
+            item = catObject[key];
+            listItem = Spektral.createNewElement("li", "item" + itemNum, catList);
+            listItem.innerHTML = item.title;
+            Spektral.attachEventListener(listItem, "click", onListItemClick);
+            itemNum += 1;
+        }
+    }
+
+    ////////////////////
+    ////ON LIST ITEM CLICK
+    ////////////////////
+    function onListItemClick(e) {
+
+        //scrollToTop(e);
+        var
+            target = Spektral.getTarget(e),
+            targetID = Spektral.getTargetID(e),
+            cat = Spektral.getParent(target).id,
+            num = Spektral.stripString(targetID, "item");
     }
 
 }(window));
